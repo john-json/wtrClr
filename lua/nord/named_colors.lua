dbcdcb1fd4c0027f40bb0b81759d7fcfe720149a
John json <37014671+john-json@users.noreply.github.com>
2024-06-18 11:42:20 +0200

    Update named_colors.lua

--- lua/nord/named_colors.lua
+++ lua/nord/named_colors.lua
@@ -1,22 +1,22 @@
 local nord = {
 	--16 colors
-	black = "#2E3440", -- nord0 in palette
-	dark_gray = "#3B4252", -- nord1 in palette
-	gray = "#434C5E", -- nord2 in palette
-	light_gray = "#4C566A", -- nord3 in palette
-	light_gray_bright = "#616E88", -- out of palette
-	darkest_white = "#D8DEE9", -- nord4 in palette
-	darker_white = "#E5E9F0", -- nord5 in palette
-	white = "#ECEFF4", -- nord6 in palette
-	teal = "#8FBCBB", -- nord7 in palette
-	off_blue = "#88C0D0", -- nord8 in palette
+	black = "#1c1c1c", -- nord0 in palette
+	dark_gray = "#252525", -- nord1 in palette
+	gray = "#5d5d5d", -- nord2 in palette
+	light_gray = "#4d4d4d", -- nord3 in palette
+	light_gray_bright = "#8b9094", -- out of palette
+	darkest_white = "#b8b8b5", -- nord4 in palette
+	darker_white = "#e3e3e3", -- nord5 in palette
+	white = "#ebebeb", -- nord6 in palette
+	teal = "#778974", -- nord7 in palette
+	off_blue = "#606f76", -- nord8 in palette
 	glacier = "#81A1C1", -- nord9 in palette
-	blue = "#5E81AC", -- nord10 in palette
-	red = "#BF616A", -- nord11 in palette
-	orange = "#D08770", -- nord12 in palette
-	yellow = "#EBCB8B", -- nord13 in palette
-	green = "#A3BE8C", -- nord14 in palette
-	purple = "#B48EAD", -- nord15 in palette
+	blue = "#415168", -- nord10 in palette
+	red = "#823e39", -- nord11 in palette
+	orange = "#ae4c39", -- nord12 in palette
+	yellow = "#c7884", -- nord13 in palette
+	green = "#7f984b", -- nord14 in palette
+	purple = "#714770", -- nord15 in palette
 	none = "NONE",
 }
 return nord
